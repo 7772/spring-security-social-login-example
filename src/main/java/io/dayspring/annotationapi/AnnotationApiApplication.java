16b604d2722ac477cdf71b3f072e45808224909b
박현도 <hyundo@wemakeprice.com>
2021-11-26 09:15:03 +0900

    initialize project

---
+++ src/main/java/io/dayspring/annotationapi/AnnotationApiApplication.java
@@ -0,0 +1,13 @@
+package io.dayspring.annotationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnotationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnotationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/dayspring/annotationapi/AnnotationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dayspring.annotationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnotationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
